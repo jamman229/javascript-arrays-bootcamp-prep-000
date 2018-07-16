2560b970a3cc7237db2541125708fc2a08fba443
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-16 03:30:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,20 +1,13 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-var superheroines = ['catwoman', 'she-hulk', 'mystique']
-superheroines.push('wonder woman')
-
-var cities = ['New York', 'San Francisco']
-cities.unshift('Philadelphia')
 
 function addElementToBeginningOfArray (array, element){
-  var array = new Array()
+  var array = new Array[];
   array.unshift("element")
   return array
 }\
 
-function destructivelyAddElementToBeginningOfArray (array, element)
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift("element")
+  return array
+}
+
