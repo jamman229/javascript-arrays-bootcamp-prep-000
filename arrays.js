fc386783ea51bd4886accc97e510a0780b5aeb23
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-16 03:18:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,5 @@ var ingredient6 = "garlic"
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var superheroines = ['catwoman', 'she-hulk', 'mystique']
 superheroines.push('wonder woman')
+var cities = ['New York', 'San Francisco']
+cities.unshift('Philadelphia')
